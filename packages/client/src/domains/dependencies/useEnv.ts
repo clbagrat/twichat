8c13a7fe2130b517cf63cf09d2890536ef8f5724
clbagrat <gobedashvilibagrat@gmail.com>
2022-10-10 22:22:27 +0200

    error handling for env vars

--- packages/client/src/domains/dependencies/useEnv.ts
+++ packages/client/src/domains/dependencies/useEnv.ts
@@ -5,5 +5,9 @@ import { useDependency } from "./dependency";
 export const useEnv = (name: keyof ClientEnvVars): string => {
   const envVars = useDependency<Record<string, string>>("envVarStorage");
 
+  if (!envVars[name]) {
+    throw new Error(`Env variable ${name} is not provided`)
+  }
+
   return envVars[name];
 }
